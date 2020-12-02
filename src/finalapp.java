ebb8b6a96c95884501ae066486512feb29d9b39d
Cameron Longfellow <clongfellow@hawk.iit.edu>
2020-12-01 20:02:08 -0600

    improved database structure, added stuff

--- src/finalapp.java
+++ src/finalapp.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Random;
 import java.util.Scanner;
-
+//need to populate database with spots/lots 
 public class finalapp {
 	 static final String JDBC_DRIVER = "org.postgresql.Driver";
 	 static final String DB_URL = "jdbc:postgresql://localhost:5432/finproj";
@@ -22,14 +22,6 @@ public class finalapp {
 		      USER = scan.nextLine();
 			  System.out.println("input password: ");
 			  PASS = scan.nextLine();
-//				   System.out.println("input username: ");
-//				   USER = scan.nextLine();
-//				   System.out.println("input password: ");
-//				   PASS = scan.nextLine();
-//				   System.out.println("input role: member/non-member/staff/admin (m/n/s/a) ");
-//				   String role = scan.nextLine();
-//				   sql = "CREATE USER" + USER + "WITH PASSWORD"+PASS+";";
-
 		      //STEP 3: Open a connection
 		      System.out.println("Connecting to a selected database...");
 		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -44,6 +36,15 @@ public class finalapp {
 		    	  System.out.println(name);
 		    	  if (name.equals("admin")){
 		    		  adminDash();
+		    	  }
+		    	  if (name.equals("non_member")){
+		    		  nonmemberDash();
+		    	  }
+		    	  if (name.equals("a_member")){
+		    		  memberDash();
+		    	  }
+		    	  if (name.equals("staff")){
+		    		  staffDash();
 		    	  }
 		      }
 		      rs.close();
@@ -103,20 +104,55 @@ public class finalapp {
 		    	String endTime = scan.nextLine();
 		    	makeReservation(cname, "drivein", lotNum, spotNum, startTime, endTime);
 		    }
+		    if (input.equals("um")) {
+		    	System.out.println("enter username to have membership updated:");
+		    	String cname = scan.nextLine();
+		    	System.out.println("change to member or nonmember: (m, n)");
+		    	String member = scan.nextLine();
+		    	changeMembership(cname, member);
+		    }
 	   }
 	   static void makeReservation(String cname, String type, int lotNum, int spotNum, String startTime, String endTime) throws SQLException{
 		   Random rnd = new Random();
 		   //need query to check if unique
+		   //need to check if timeslot available for spot
 		   int n = 10000 + rnd.nextInt(90000);
+		   System.out.println("Attempting reservation...");
 		   if(type.equals("drivein")){
-			   stmt = conn.prepareStatement("insert into reservations(drive_in, start_time, end_time, res_num)\r\n"
-				   		+ "VALUES (?, ?, ?, ?);");
+			   stmt = conn.prepareStatement("insert into reservations(drive_in, start_time, end_time, res_num, lot_id, spot_num)\r\n"
+				   		+ "VALUES (?, ?, ?, ?);"
+				   		+ "update spots set available = false where spot_num = ? and lot_id = ?");
 				   stmt.setBoolean(1, true);
 				   stmt.setTimestamp(2, java.sql.Timestamp.valueOf("startTime"));
 				   stmt.setTimestamp(3, java.sql.Timestamp.valueOf("endTime"));
 				   stmt.setInt(4, n);
+				   stmt.setInt(5, lotNum);
+				   stmt.setInt(6, spotNum);
+				   stmt.setInt(7, spotNum);
+				   stmt.setInt(8, lotNum);
+				   stmt.execute();
 		   }
-		   
-	   }
-	   }//end finalapp
+		   System.out.println("Reservation Complete!");
+	   }//end makeReservation
+	   static void changeMembership(String user, String member) throws SQLException {
+		   if(member.equals("m")) {
+			   stmt = conn.prepareStatement("update users set is_role = a_member where username = ?;"
+			   		+ "revoke non_member from ?;"
+			   		+ "grant a_member to ?");
+			   stmt.setString(1, user);
+			   stmt.setString(2, user);
+			   stmt.setString(3, user);
+			   stmt.execute();
+		   }
+		   if(member.equals("n")) {
+			   stmt = conn.prepareStatement("update users set is_role = non_member where username = ?;"
+			   		+ "revoke a_member from ?;"
+			   		+ "grant non_member to ?");
+			   stmt.setString(1, user);
+			   stmt.setString(2, user);
+			   stmt.setString(3, user);
+			   stmt.execute();
+	       }
+	    }//end changeMembership
+}//end finalapp
 
--- src/initquery.sql
+++ src/initquery.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF exists lots CASCADE;
+DROP TABLE IF EXISTS spots CASCADE;
+DROP TABLE IF EXISTS reservation CASCADE;
 create table users (
 	username char (20),
 	is_role char(20), 
@@ -12,26 +16,30 @@ create table lots (
 );
 create table spots (
 	spot_num int not null, 
-	lot_id int,
+	lot_id int NOT null,
 	reserved_by char(20),
-	primary key (spot_num),
-	foreign key (lot_id) references lots(lot_id),
-	foreign key (reserved_by) references users(username)
+	available bool,
+	primary key (spot_num, lot_id),
+	foreign key (lot_id) references lots(lot_id)
 );
 create table reservation (
 	member bool, 
 	drive_in bool,
 	online bool,
+	spot_num int not null, 
+	lot_id int NOT null,
 	start_time timestamp,
 	end_time timestamp,
 	res_num int not null, 
-	primary key (res_num)
+	primary key (res_num),
+	foreign key (spot_num, lot_id) REFERENCES spots(spot_num, lot_id)
 );
 create role a_member;
 create role non_member;
 create role staff;
 create role admin;
 ALTER ROLE admin WITH superuser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO admin;
 CREATE USER joe WITH password 1234;
 GRANT admin TO joe; 
 
